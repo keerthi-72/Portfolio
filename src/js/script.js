fe5b5d97829491821082fc20b18d1c61bc43afaf
keerthi-72 <palagirikeerthi04@gmail.com>
2026-01-28 09:45:51 +0530

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+function showAvatar(){
+document.getElementById("avatarImg").innerHTML =
+'<img src="https://rukminim2.flixcart.com/image/480/480/kxjav0w0/poster/p/a/i/medium-rapunzel-home-princess-animation-cartoon-disney-matte-original-imag9yvgakpxrzws.jpeg?q=90" width="300">';
+}
+function showCricket(){
+document.getElementById("hobbyImg").innerHTML =
+'<img src="https://i.pinimg.com/564x/92/14/14/9214148d71059266d22b8383c7cf7606.jpg" width="300">';
+}
+function showMusic(){
+document.getElementById("hobbyImg").innerHTML =
+'<img src="https://i.pinimg.com/564x/92/14/14/9214148d71059266d22b8383c7cf7606.jpg" width="300">';
+}
